8428a5552fcc2d1a76f1a948e08611111ec38738
Soufiane <62034446+gitsoufiane@users.noreply.github.com>
2023-03-18 09:38:11 -0700

    getbyrole option

--- src/components/application/Application.test.js
+++ src/components/application/Application.test.js
@@ -5,8 +5,21 @@ describe("Application", () => {
   test("renders Correctly", () => {
     render(<Application />);
 
-    const nameElement = screen.getByRole("textbox");
+    const pageHeading = screen.getByRole("heading", {
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole("heading", {
+      level: 2,
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
+    const nameElement = screen.getByRole("textbox", { name: /name/i });
     expect(nameElement).toBeInTheDocument();
+
+    const bioElement = screen.getByRole("textbox", { name: /bio/i });
+    expect(bioElement).toBeInTheDocument();
 
     const jobLocationElement = screen.getByRole("combobox");
     expect(jobLocationElement).toBeInTheDocument();
