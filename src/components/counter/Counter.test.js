be80d2a3270be710a70787d6d7f05277ca5d06ad
Soufiane <62034446+gitsoufiane@users.noreply.github.com>
2023-03-20 15:41:42 -0700

    add file for provider and component  to test

--- src/components/counter/Counter.test.js
+++ src/components/counter/Counter.test.js
@@ -43,4 +43,41 @@ describe("Counter", () => {
     const headerElement = screen.getByRole("heading", { level: 1 });
     expect(headerElement).toHaveTextContent(2);
   });
+
+  test("render a count of 10 after clicking on the set button", async () => {
+    user.setup();
+    render(<Counter />);
+
+    const inputElement = screen.getByRole("spinbutton");
+    await user.type(inputElement, "10");
+
+    expect(inputElement).toHaveValue(10);
+
+    const setButton = screen.getByRole("button", { name: /set/i });
+    await user.click(setButton);
+
+    const headerElement = screen.getByRole("heading", { level: 1 });
+    expect(headerElement).toHaveTextContent("10");
+  });
+
+  test("element are focused in the right order", async () => {
+    user.setup();
+    render(<Counter />);
+
+    const incrementButton = screen.getByRole("button", {
+      name: /increment/i,
+    });
+    const inputElement = screen.getByRole("spinbutton");
+
+    const setButton = screen.getByRole("button", { name: /set/i });
+
+    await user.tab();
+    expect(incrementButton).toHaveFocus();
+
+    await user.tab();
+    expect(inputElement).toHaveFocus();
+
+    await user.tab();
+    expect(setButton).toHaveFocus();
+  });
 });
