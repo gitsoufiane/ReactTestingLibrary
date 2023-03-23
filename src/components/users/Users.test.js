b34f96a41152d2e87a949afef1b8464fa4527299
Soufiane <62034446+gitsoufiane@users.noreply.github.com>
2023-03-23 09:32:20 -0700

    handle error

--- src/components/users/Users.test.js
+++ src/components/users/Users.test.js
@@ -1,5 +1,7 @@
 import { Users } from "./Users";
 import { render, screen } from "@testing-library/react";
+import { server } from "../../mocks/server";
+import { rest } from "msw";
 
 describe("Users", () => {
   test("Render correctly", () => {
@@ -7,9 +9,22 @@ describe("Users", () => {
     expect(screen.getByText(/users/i)).toBeInTheDocument();
   });
 
-  test("render a list of users ", async () => {
+  test("render a list of users", async () => {
     render(<Users />);
     const users = await screen.findAllByRole("listitem");
     expect(users).toHaveLength(3);
+  });
+  test("render error", async () => {
+    server.use(
+      rest.get(
+        "https://jsonplaceholder.typicode.com/users",
+        (req, res, ctx) => {
+          return res(ctx.status(500));
+        }
+      )
+    );
+    render(<Users />);
+    const users = await screen.findByText(/Error fetching users/i);
+    expect(users).toBeInTheDocument();
   });
 });
