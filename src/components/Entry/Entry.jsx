2cd9793e492f59c94de4b28d7ab5cb74df3ab4a0
Soufiane <62034446+gitsoufiane@users.noreply.github.com>
2023-04-16 00:56:27 -0700

    ✨ feat(Entry.jsx): add Entry component 🔥 chore(SundaesOnDemand.test.jsx): delete SundaesOnDemand test file ✨ feat(handlers.js): add mock for scoops endpoint in handlers

---
+++ src/components/Entry/Entry.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Entry() {
+  return <div>Entry</div>;
+}
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -9,4 +9,13 @@ export const handlers = [
       ctx.json([{ name: "soufiane" }, { name: "luisa" }, { name: "charlotte" }])
     );
   }),
+  rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        { name: "soufiane", imagePath: "/images/soufiane.png" },
+        { name: "luisa", imagePath: "/images/luisa.png" },
+      ])
+    );
+  }),
 ];
